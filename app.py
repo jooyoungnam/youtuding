d8c67eb70335bdf21d92a6d16729da26c3bf3846
whitelang <143645324+jooyoungnam@users.noreply.github.com>
2024-08-18 21:18:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,18 +14,17 @@ def download():
     format = request.form['format']
     
     ydl_opts = {
-        'format': 'bestvideo+bestaudio/best' if format == 'mp4' else 'bestaudio/best',
-        'outtmpl': 'downloads/%(title)s.%(ext)s',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }] if format == 'mp3' else [],
-        'ffmpeg_location': '/usr/bin/ffmpeg',  # 서버에서 ffmpeg 경로
-        'cookiefile': '/path/to/cookies.txt',  # 쿠키 파일 경로
-        'no-cache-dir': True,  # 캐시 비활성화
-        'restrictfilenames': True  # 파일 이름에서 특수 문자 제거
-    }
+    'cookiefile': 'cookies.txt',  # 프로젝트 루트 디렉토리에 있는 cookies.txt 파일을 참조
+    'format': 'bestvideo+bestaudio/best' if format == 'mp4' else 'bestaudio/best',
+    'outtmpl': 'downloads/%(title)s.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }] if format == 'mp3' else [],
+    'ffmpeg_location': '/usr/bin/ffmpeg'
+}
+
     
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url, download=True)
