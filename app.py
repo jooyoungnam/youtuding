8a25a5e21149044a520d7639a5ae17959e5004db
whitelang <143645324+jooyoungnam@users.noreply.github.com>
2024-08-21 04:58:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import os
-import yt_dlp as youtube_dl
 from flask import Flask, request, render_template, send_file, redirect, url_for
+import yt_dlp as youtube_dl
+import os
+import shutil
 from werkzeug.utils import secure_filename
 import glob
 
@@ -15,10 +16,17 @@ def download():
     url = request.form['url']
     format = request.form['format']
     
+    temp_dir = 'temp_downloads'
+    final_dir = 'downloads'
+    
+    # 임시 디렉토리 생성
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
+    
     try:
         ydl_opts = {
             'format': 'bestvideo+bestaudio/best' if format == 'mp4' else 'bestaudio/best',
-            'outtmpl': 'downloads/%(title)s.%(ext)s',
+            'outtmpl': os.path.join(temp_dir, '%(title)s.%(ext)s'),
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
@@ -30,7 +38,7 @@ def download():
         
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=True)
-            original_filename_pattern = os.path.join('downloads', f"{info_dict['title']}*")
+            original_filename_pattern = os.path.join(temp_dir, f"{info_dict['title']}*")
             
             # 파일 패턴으로 파일 찾기
             downloaded_files = glob.glob(original_filename_pattern)
@@ -41,15 +49,19 @@ def download():
             original_filename = downloaded_files[0]
             base_filename = os.path.splitext(original_filename)[0]  # 확장자를 제외한 파일명
             final_name = secure_filename(f"{base_filename}.{format}")  # 최종 파일명과 확장자
+            final_path = os.path.join(final_dir, final_name)
             
-            # 파일 권한 설정
-            os.chmod(original_filename, 0o666)  # 읽기 및 쓰기 권한 설정
-            os.rename(original_filename, final_name)  # 다운로드된 파일을 확장자에 맞게 이름 변경
+            # 권한을 수정하여 임시 디렉토리에서 최종 디렉토리로 파일 이동
+            shutil.move(original_filename, final_path)
         
-        return redirect(url_for('download_file', filename=os.path.basename(final_name)))
+        return redirect(url_for('download_file', filename=os.path.basename(final_path)))
     
     except Exception as e:
         return f"오류가 발생했습니다: {str(e)}", 500
+    
+    finally:
+        # 임시 디렉토리 삭제
+        shutil.rmtree(temp_dir, ignore_errors=True)
 
 @app.route('/download-file/<filename>')
 def download_file(filename):
