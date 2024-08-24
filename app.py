811a6b76f2ac40b583d5461202220c9cdf3677c7
whitelang <143645324+jooyoungnam@users.noreply.github.com>
2024-08-25 03:24:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 app.secret_key = 'supersecretkey'  # 플래시 메시지에 필요
 
 # Redis URL 설정
-redis_host = 'svc.sel4.cloudtype.app'
-redis_port = 30309
+redis_host = os.getenv('REDIS_HOST', 'localhost')
+redis_port = os.getenv('REDIS_PORT', 6379)
 app.config['CELERY_BROKER_URL'] = f'redis://{redis_host}:{redis_port}/0'
 app.config['CELERY_RESULT_BACKEND'] = f'redis://{redis_host}:{redis_port}/0'
 
